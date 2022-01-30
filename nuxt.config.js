dcf046e4ac5470970ebdf5091babfc275373efc0
leotabosa <leonardoctbraga@gmail.com>
2022-01-30 17:43:35 -0300

    fix(config): fix target position

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,11 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
+  target: 'static',
   head: {
     title: 'wordlezin',
     htmlAttrs: {
       lang: 'en',
     },
-    target: 'static',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
